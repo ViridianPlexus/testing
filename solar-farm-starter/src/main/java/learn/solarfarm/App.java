b10707651a41e076e80fc0bb85d47c1381d5769d
ViridianPlexus <viridian@Viridian>
2024-11-11 11:59:35 +0100

    ok

---
+++ solar-farm-starter/src/main/java/learn/solarfarm/App.java
@@ -0,0 +1,12 @@
+package learn.solarfarm;
+
+import learn.solarfarm.data.SolarPanelFileRepository;
+import learn.solarfarm.data.SolarPanelRepository;
+
+public class App {
+    public static void main(String[] args) {
+
+        SolarPanelRepository repository = new SolarPanelFileRepository();
+
+    }
+}
